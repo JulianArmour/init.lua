ee7093a0ede29fefa9f6760214577310174065cf
juarmour <juarmour@cisco.com>
2024-05-01 19:41:31 -0400

    nvim surround plugin

--- init.lua
+++ init.lua
@@ -186,6 +186,14 @@ require('lazy').setup({
   --     show_trailing_blankline_indent = false,
   --   },
   -- },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end
+  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
