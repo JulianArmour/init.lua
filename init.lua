7c5e75ebc556f7dbcc95021a06621343ef9d7cd6
JulianArmour <julian.armour@videotron.ca>
2025-06-19 19:57:41 -0400

    structure my options

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ end
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+require('options')
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -219,64 +221,6 @@ require('lazy').setup({
   { import = 'custom.plugins' },
 })
 
--- [[ Setting options ]]
--- See `:help vim.o`
-
-vim.o.scrolloff = 3
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
-vim.o.number = true
-vim.o.relativenumber = true
-
-vim.o.colorcolumn = "100"
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = 0
-vim.o.softtabstop = -1
-vim.o.expandtab = true
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- python indentation, the way I like it
--- vim.g.python_indent = {
---
--- }
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect,popup'
-vim.o.pumheight = 10
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
 
