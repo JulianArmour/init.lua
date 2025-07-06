c04fbad77c37a2be29495b91344680f92fe338a1
JulianArmour <julian.armour@videotron.ca>
2025-07-06 14:15:50 -0400

    open help vertically, always

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,9 +1,22 @@
 -- highlight what I yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+-- open help window vertically to the right.
+local help_group = vim.api.nvim_create_augroup('HelpWindow', {})
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(ev)
+    local buffer_type = vim.api.nvim_get_option_value('buftype', {buf = ev.buf})
+    if buffer_type == "help" then
+      vim.cmd('wincmd L')
+    end
+  end,
+  group = help_group,
+  pattern = '*.txt',
 })
